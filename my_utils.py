d8a4fb72d0db5f9b4d2f48d424901c9529833c88
Kambakhsh Eskandari <kambakhsh.eskandari1@gmail.com>
2023-04-10 14:29:39 +0430

    some changes

--- my_utils.py
+++ my_utils.py
@@ -5,23 +5,23 @@ import numpy as np
 def log_img(image):
     # Apply log transformation method
     img = image.copy()
-    log_image =  (np.log(img + 1)) / np.log(1 + np.max(img))
+    log_image =  255 * (np.log(img + 1)) / np.log(1 + np.max(img))
 
 
 
 
     # Specify the data type so that
     # float value will be converted to int
-    # log_image= np.array(log_image, dtype = np.uint8)
+    
 
     for x in range(log_image.shape[0]):
         for y in range(log_image.shape[1]):
             if log_image[x][y] < 0:
                 log_image[x][y] = 0
-            # elif log_image[x][y] > 255:
-            #     log_image[x][y] = 255
+            elif log_image[x][y] > 255:
+                log_image[x][y] = 255
     
-    
+    log_image= np.array(log_image, dtype = np.uint8)
     return log_image
     
                 
